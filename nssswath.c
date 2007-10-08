8bf9e0aab8c5b49c5a897e3b66267db9b66a4ba3
Oliver Lemke <olemke@core-dump.info>
2007-10-08 12:19:14 +0000

    * print accumulated size of gaps.
    * print the number of ignored gaps.
    
    
    git-svn-id: https://www.sat.ltu.se/svn/external/nsscheck/trunk@23 f7e51615-772b-0410-8683-eb109a7a44ae

--- nssswath.c
+++ nssswath.c
@@ -138,12 +138,14 @@ nss_build_swath_list (FILE *fp, int verbose)
 void
 nss_detect_gaps (const nss_swath_list *swath_list, int gapsize, int refine)
 {
+  long gapcount = 0;
+  long gapignorecount = 0;
+  time_t gaptotal = 0;
   nss_swath_list *cur = (nss_swath_list *)swath_list;
 
   while (cur->next && cur->next->swath)
     {
-      if (cur->next->swath->stime > cur->swath->etime
-          && cur->next->swath->stime - cur->swath->etime >= gapsize*60)
+      if (cur->next->swath->stime > cur->swath->etime)
         {
           nss_swath_data gap;
           nss_swath_list *it;
@@ -181,24 +183,52 @@ nss_detect_gaps (const nss_swath_list *swath_list, int gapsize, int refine)
 
           if (gap.etime - gap.stime > 0)
             {
-              char timestr[1024];
+              if (gap.etime - gap.stime >= gapsize * 60)
+                {
+                  char timestr[1024];
 
-              strftime (timestr, 1024, "%Y-%m-%d %H:%M", gmtime (&gap.stime));
-              printf ("Gap between %s", timestr);
+                  strftime (timestr, 1024, "%Y-%m-%d %H:%M", gmtime (&gap.stime));
+                  printf ("Gap between %s", timestr);
 
-              strftime (timestr, 1024, "%Y-%m-%d %H:%M", gmtime (&gap.etime));
-              printf (" and %s, %ld mins\n", timestr, (gap.etime - gap.stime) / 60);
+                  strftime (timestr, 1024, "%Y-%m-%d %H:%M", gmtime (&gap.etime));
+                  printf (" and %s, %ld mins\n", timestr, (gap.etime - gap.stime) / 60);
+                  gapcount++;
+                  gaptotal += gap.etime - gap.stime;
+                }
+              else
+                gapignorecount++;
             }
         }
 
       cur = cur->next;
     }
 
+  if (gapcount)
+    {
+      time_t hours;
+      time_t mins;
+
+      hours = gaptotal / (60 * 60);
+      mins = (gaptotal / 60) - hours * 60;
+      printf ("\n");
+      if (hours)
+        printf ("%ld gaps with a total length of %ld hours and %ld minutes found.\n",
+                gapcount, hours, mins);
+      else
+        printf ("%ld gaps with a total length of %ld minutes found.\n\n",
+                gapcount, mins);
+    }
+
   if (gapsize)
     {
-      printf ("\n");
-      printf ("Gaps smaller than %d minutes ignored.\n", gapsize);
+      if (gapignorecount)
+        printf ("%ld gaps smaller than %d minutes ignored.\n",
+                gapignorecount, gapsize);
+      else
+        printf ("No gaps smaller than %d minutes ignored.\n", gapsize);
     }
+
+  printf ("\n");
 }
 
 
@@ -332,7 +362,7 @@ void nss_print_info (const nss_swath_list *swath_list)
       printf ("Start time: %s\n", timestr);
       strftime (timestr, 1024, "%Y-%m-%d %H:%M", gmtime (&etime));
       printf ("End time  : %s\n", timestr);
-      printf ("# of files: %ld\n", count);
+      printf ("# of files: %ld\n\n", count);
     }
 }
 
@@ -348,7 +378,7 @@ void nss_print_swath_list (const nss_swath_list *swath_list)
       strftime (timestr, 1024, "%Y-%m-%d %H:%M", gmtime (&cur->swath->stime));
       printf ("Start time: %s", timestr);
       strftime (timestr, 1024, "%Y-%m-%d %H:%M", gmtime (&cur->swath->etime));
-      printf (" - End time  : %s\n", timestr);
+      printf (" - End time: %s\n", timestr);
 
       cur = cur->next;
     }
